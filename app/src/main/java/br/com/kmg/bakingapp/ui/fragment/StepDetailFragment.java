a978353e30cdd11536df69b62acae37e1edb9a0f
Klinsman Maia <klinsman.goncalves@gmail.com>
2019-12-17 01:39:13 -0400

    Adding exoplayer.

--- app/src/main/java/br/com/kmg/bakingapp/ui/fragment/StepDetailFragment.java
+++ app/src/main/java/br/com/kmg/bakingapp/ui/fragment/StepDetailFragment.java
@@ -1,5 +1,7 @@
 package br.com.kmg.bakingapp.ui.fragment;
 
+import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,29 +12,58 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
+
 import br.com.kmg.bakingapp.R;
 import br.com.kmg.bakingapp.model.ReceiptStep;
 
 public class StepDetailFragment extends Fragment {
 
-    private final String RECEIP_STEP_STATE = "receipt_step_key";
+    private final String RECEIPT_STEP_STATE = "receipt_step_key";
     private ReceiptStep receiptStep;
 
+    private SimpleExoPlayer mExoPlayer;
+    private SimpleExoPlayerView mPlayerView;
+    private Context mContext;
+
     public StepDetailFragment() {
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        mContext = context;
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_step_detail, container, false);
-        TextView tvShortName = view.findViewById(R.id.tv_short_name_step);
         TextView tvStepDescription = view.findViewById(R.id.tv_step_description);
-        if(savedInstanceState != null && savedInstanceState.containsKey(RECEIP_STEP_STATE)){
-            receiptStep = (ReceiptStep) savedInstanceState.getSerializable(RECEIP_STEP_STATE);
+        mPlayerView = view.findViewById(R.id.playerView);
+
+        if(savedInstanceState != null && savedInstanceState.containsKey(RECEIPT_STEP_STATE)){
+            receiptStep = (ReceiptStep) savedInstanceState.getSerializable(RECEIPT_STEP_STATE);
         }
 
-        tvShortName.setText(receiptStep.getShortDescription());
         tvStepDescription.setText(receiptStep.getDescription());
+        if(receiptStep.getVideoURL() != null){
+            initializePlayer(Uri.parse(receiptStep.getVideoURL()), mContext);
+        } else if (receiptStep.getThumbnailURL() != null){
+            initializePlayer(Uri.parse(receiptStep.getThumbnailURL()), mContext);
+        }
+
         return view;
     }
 
@@ -47,6 +78,23 @@ public class StepDetailFragment extends Fragment {
     @Override
     public void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
-        outState.putSerializable(RECEIP_STEP_STATE, receiptStep);
+        outState.putSerializable(RECEIPT_STEP_STATE, receiptStep);
     }
+
+    private void initializePlayer(Uri mediaUri, Context context) {
+        if (mExoPlayer == null) {
+            // Create an instance of the ExoPlayer.
+            TrackSelector trackSelector = new DefaultTrackSelector();
+            LoadControl loadControl = new DefaultLoadControl();
+            mExoPlayer = ExoPlayerFactory.newSimpleInstance(context, trackSelector, loadControl);
+            mPlayerView.setPlayer(mExoPlayer);
+
+            // Prepare the MediaSource.
+            String userAgent = Util.getUserAgent(context, "BakingAppUdacity");
+            MediaSource mediaSource = new ExtractorMediaSource(mediaUri, new DefaultDataSourceFactory(context, userAgent), new DefaultExtractorsFactory(), null, null);
+            mExoPlayer.prepare(mediaSource);
+            mExoPlayer.setPlayWhenReady(true);
+        }
+    }
+
 }
